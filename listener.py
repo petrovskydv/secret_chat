33338bb20827be85019d80217c2a8ca77940d8ba
denis <petrovsky.dv@gmail.com>
2023-01-18 19:06:32 +0300

    add close connection

--- listener.py
+++ listener.py
@@ -8,12 +8,14 @@ import configargparse
 async def listen_chat(host, port, log_path):
     reader, writer = await asyncio.open_connection(host, port)
 
-    while received_data := await reader.readline():
-        # TODO добавить защиту от потери сетевого подключения
-        text = f'[{datetime.now().strftime("%d.%m.%y %H:%M")}]: {received_data.decode()}'
+    while message := await reader.readline():
+        text = f'[{datetime.now().strftime("%d.%m.%y %H:%M")}]: {message.decode()}'
         print(text)
         async with aiofiles.open(log_path, mode='a') as f:
             await f.write(text)
+
+    writer.close()
+    await writer.wait_closed()
 
 
 if __name__ == '__main__':
@@ -22,7 +24,7 @@ if __name__ == '__main__':
         ignore_unknown_config_file_keys=True,
         description='Listener for dvmn chat',
     )
-    parser.add_argument('-c', '--my-config', is_config_file=True, help='config file path')
+    parser.add_argument('-c', '--config', is_config_file=True, help='config file path')
     parser.add_argument('--host', required=True, help='chat server url')
     parser.add_argument('--port', required=True, help='chat server port')
     parser.add_argument('--log_path', required=True, help='path to chat logs')
