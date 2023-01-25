16e9f68aeb141749f25f2703de219379232e3a61
denis <petrovsky.dv@gmail.com>
2023-01-25 21:10:44 +0300

    add reconect decorator

--- utils/tools.py
+++ utils/tools.py
@@ -1,5 +1,7 @@
 import asyncio
+import functools
 import logging
+import textwrap
 from contextlib import asynccontextmanager, suppress
 
 logger = logging.getLogger('sender')
@@ -34,3 +36,34 @@ async def get_connection(host, port):
             logger.debug(f'close connection {host}:{port}')
             writer.close()
             await writer.wait_closed()
+
+
+def reconnect(
+        start_sleep_time: float = 0.1,
+        factor: int = 2,
+        border_sleep_time: float = 10,
+        exceptions: tuple = Exception,
+        logger: logging.Logger = None
+):
+    def wrapper(func):
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs):
+            _tries = 1
+            _delay = start_sleep_time
+            while True:
+                try:
+                    return await func(*args, **kwargs)
+                except exceptions as e:
+                    _tries += 1
+                    _delay = min(start_sleep_time * (factor ** _tries), border_sleep_time)
+                    msg = f"""\
+                    Function: {func.__name__}
+                    Exception: {e}
+                    Retrying in {_delay} seconds!"""
+                    if logger:
+                        logger.debug(textwrap.dedent(msg))
+                    await asyncio.sleep(_delay)
+
+        return wrapped
+
+    return wrapper
